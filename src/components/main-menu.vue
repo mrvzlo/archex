a391e26eab3d3e92ea2a10ed46593d5c2dbfe040
Vladimir Andrejev <andrejevve@gmail.com>
2024-12-25 01:24:44 +0200

    Remove start button

--- src/components/main-menu.vue
+++ src/components/main-menu.vue
@@ -1,9 +1,9 @@
 <template>
    <div class="menu">
-      <h1>ARCHEX EMPIRE</h1>
-      <button v-if="!show.levels" v-on:click="show.levels = true">{{ $t('startGame') }}</button>
+      <h1>ARCHEX EMPIRES</h1>
+      <h2>{{ $t('startGame') }}</h2>
 
-      <div class="levels" v-if="show.levels">
+      <div class="levels">
          <div class="label">{{ $t('empires.0') }}</div>
          <div v-on:click="selectMap('1')" class="level">
             <img src="../assets/maps/1.jpg" />
@@ -12,6 +12,7 @@
       </div>
    </div>
 
+   <div class="toolbar"></div>
    <div class="made-by">Made by AndrejevVE</div>
 </template>
 
@@ -19,6 +20,5 @@
 import { defineProps, reactive } from 'vue';
 
 const props = defineProps({ select: Function } as any);
-const show = reactive({ levels: false });
 const selectMap = (name: string) => props.select(name);
 </script>
